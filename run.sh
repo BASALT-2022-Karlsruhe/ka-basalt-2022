dcb92e3c1c99ba20da8114d8946e49f868f788fd
Walter Laurito <Lauritowal@yahoo.com>
2022-08-16 20:32:50 +0200

    add basic files

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# get variables from .env
+export $(cat .env | xargs)
+
+# create folders for shared volumes
+mkdir -p data
+
+# generate requirements.txt
+pip-compile setup.py
+
+# build and run container
+docker-compose up --build -d
+
+# start bash on container
+docker exec -it --user root ${CONTAINER_NAME} /bin/bash
