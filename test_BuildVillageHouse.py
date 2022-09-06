a96232ac76873e31a6c502f88c3b77516e9e8817
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-09-06 10:56:07 +0200

    Fix video creation

--- test_BuildVillageHouse.py
+++ test_BuildVillageHouse.py
@@ -2,7 +2,9 @@ from run_agent import main as run_agent_main
 from config import EVAL_EPISODES, EVAL_MAX_STEPS
 from pathlib import Path
 def main():
-    video_dir = Path("./video/build_village_house").mkdir(parents=True, exist_ok=True)
+    video_dir = Path("./video/build_village_house")
+    video_dir.mkdir(parents=True, exist_ok=True)
+
     run_agent_main(
         model="data/VPT-models/foundation-model-1x.model",
         weights="train/MineRLBasaltBuildVillageHouse.weights",
