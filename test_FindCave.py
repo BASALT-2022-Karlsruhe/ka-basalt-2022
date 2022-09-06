2fa853ecefc500c4662d188ca0345628b6480375
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-09-06 09:41:16 +0200

    Some changes

--- test_FindCave.py
+++ test_FindCave.py
@@ -1,14 +1,16 @@
 from run_agent import main as run_agent_main
 from config import EVAL_EPISODES, EVAL_MAX_STEPS
+from pathlib import Path
 
 def main():
+    video_dir = Path("./video/find_cave").mkdir(parents=True,exist_ok=True)
     run_agent_main(
         model="data/VPT-models/foundation-model-1x.model",
         weights="train/MineRLBasaltFindCave.weights",
         env="MineRLBasaltFindCave-v0",
         n_episodes=EVAL_EPISODES,
         max_steps=EVAL_MAX_STEPS,
-        record=True,
+        video_dir=str(video_dir)
     )
 
 if __name__ == '__main__':
--- test_MakeWaterfall.py
+++ test_MakeWaterfall.py
@@ -1,13 +1,15 @@
 from run_agent import main as run_agent_main
 from config import EVAL_EPISODES, EVAL_MAX_STEPS
-
+from pathlib import Path
 def main():
+    video_dir = Path("./video/make_waterfall").mkdir(parents=True, exist_ok=True)
     run_agent_main(
         model="data/VPT-models/foundation-model-1x.model",
         weights="train/MineRLBasaltMakeWaterfall.weights",
         env="MineRLBasaltMakeWaterfall-v0",
         n_episodes=EVAL_EPISODES,
-        max_steps=EVAL_MAX_STEPS
+        max_steps=EVAL_MAX_STEPS,
+        video_dir = str(video_dir)
     )
 
 if __name__ == '__main__':
