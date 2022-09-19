129d0210024cd25b97b8b0bb43eca9cd03f3b6a1
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-09-19 17:59:35 +0200

    Update env for evalutation setting

--- test_MakeWaterfall.py
+++ test_MakeWaterfall.py
@@ -1,16 +1,23 @@
 from run_agent import main as run_agent_main
 from config import EVAL_EPISODES, EVAL_MAX_STEPS
 from pathlib import Path
+
+from utils import env_handler
+
+
 def main():
     video_dir = Path("./video/make_waterfall")
     video_dir.mkdir(parents=True, exist_ok=True)
+    env_handler.set_env(eval_episodes=5, eval_max_steps=6000)
     run_agent_main(
         model="data/VPT-models/foundation-model-1x.model",
         weights="train/MineRLBasaltMakeWaterfall.weights",
         env="MineRLBasaltMakeWaterfall-v0",
         n_episodes=EVAL_EPISODES,
         max_steps=EVAL_MAX_STEPS,
-        video_dir = str(video_dir.absolute())
+        video_dir = str(video_dir.absolute()),
+        show=True,
+        record=True
     )
 
 if __name__ == '__main__':
