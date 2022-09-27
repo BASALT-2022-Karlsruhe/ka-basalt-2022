87bfe75dd53d89ad9cbdd6c0d8776e620c1b08fd
Walter Laurito <Lauritowal@yahoo.com>
2022-09-27 18:58:36 +0200

    fix logging looking for logs subfolder before creation

--- utils/logs.py
+++ utils/logs.py
@@ -3,16 +3,21 @@ from datetime import datetime
 
 
 class Logging(object):
-  logging.basicConfig(level=logging.INFO,
-                      handlers=[
-                        logging.FileHandler(f"/home/aicrowd/train/logs/log_{datetime.now().strftime('%Y:%m:%d_%H:%M:%S')}.log"),
-                        logging.StreamHandler()
-                      ],
-                      format='%(asctime)s %(message)s'
-  )
-  logger = logging.getLogger()
+  logger = None
 
+  @staticmethod
   def info(message):
       Logging.logger.info(message)
 
+  @staticmethod
+  def setup():
+      logging.basicConfig(level=logging.INFO,
+                          handlers=[
+                              logging.FileHandler(
+                                  f"/home/aicrowd/train/logs/log_{datetime.now().strftime('%Y:%m:%d_%H:%M:%S')}.log"),
+                              logging.StreamHandler()
+                          ],
+                          format='%(asctime)s %(message)s'
+                          )
+      Logging.logger = logging.getLogger()
 
