95bc5f37e4cefaaf25c2bfac3deb02a31697f5e1
rk1a <robertklassert@pm.me>
2022-10-03 17:35:37 +0200

    Finishes implementation of SB3 and Gym wrappers

--- gym_wrappers.py
+++ gym_wrappers.py
@@ -9,12 +9,16 @@ import torch as th
 class RewardModelWrapper(gym.Wrapper):
     """Replaces the environment reward with the one obtained under a reward model"""
 
-    def __init__(self, env, reward_model, reward_model_kwargs):
+    def __init__(self, env, reward_model, reward_model_kwargs={"action_dependent": False}):
         super().__init__(env)
         self.reward_model = reward_model
         self.reward_model_kwargs = reward_model_kwargs
 
         self.last_obs = None
+
+    def update_reward_model(self, reward_model, reward_model_kwargs={"action_dependent": False}):
+        self.reward_model = reward_model
+        self.reward_model_kwargs = reward_model_kwargs
 
     def reset(self):
         initial_obs = self.env.reset()
@@ -31,8 +35,8 @@ class RewardModelWrapper(gym.Wrapper):
         return obs, reward, terminated, info
 
 
-class DictToBoxActionSpace(gym.Wrapper):
-    """Converts env with gym.Dict action space into having a gym.Box action space"""
+class DictToMultiDiscreteActionSpace(gym.Wrapper):
+    """Converts env with Dict action space into having a MultiDiscrete action space"""
 
     def __init__(self, env, minerl_agent):
         super().__init__(env)
@@ -42,76 +46,44 @@ class DictToBoxActionSpace(gym.Wrapper):
 
         self.minerl_agent = minerl_agent
 
-        self.action_space = MultiDiscrete([121, 8100])
+        # first dimension = camera, second dimension = buttons
+        self.action_space = MultiDiscrete([121, 8641])
 
-        if False:
-            self.action_space = self._convert_to_box_action_space(
-                self.env.action_space)
+        # check action space conversion
+        random_env_action = self.env.action_space.sample()
+        agent_action = self.minerl_agent._env_action_to_agent(
+            random_env_action)
+        array_action = self.to_array_action(agent_action)
+        assert array_action.squeeze() in self.action_space
 
-            # check action space conversion
-            random_action = self.env.action_space.sample()
-            numpy_action = self.dict_to_numpy(random_action)
-            assert numpy_action in self.action_space
-            dict_action = self.numpy_to_dict(numpy_action, no_batch=True)
-            assert dict_action in self.env.action_space
-            numpy_action2 = self.dict_to_numpy(dict_action)
-            dict_action2 = self.numpy_to_dict(numpy_action2, no_batch=True)
-            assert (numpy_action == numpy_action2).all()
-            for key in random_action.keys():
-                if isinstance(dict_action[key], np.ndarray):
-                    assert (dict_action[key] == dict_action2[key]).all()
-                else:
-                    assert dict_action[key] == dict_action2[key]
+        agent_action2 = self.to_agent_action(array_action)
+        env_action = self.minerl_agent._agent_action_to_env(agent_action2)
+        env_action["ESC"] = env_action["swapHands"] = env_action["pickItem"] = np.array(
+            0)
+        assert env_action in self.env.action_space
 
-    """
-    def dict_to_numpy(self, dict_action):
-        dict_numpy_action = self.action_transformer.dict_to_numpy(dict_action)
-        return np.concatenate((dict_numpy_action["buttons"], dict_numpy_action["camera"]))
+    def to_array_action(self, agent_action):
+        if isinstance(agent_action["camera"], np.ndarray):
+            array_action = np.concatenate(
+                (agent_action["camera"], agent_action["buttons"]), -1)
+        elif isintance(agent_action["camera"], th.Tensor):
+            array_action = th.cat(
+                (agent_action["camera"], agent_action["buttons"]), dim=-1)
+        return array_action
 
-    def numpy_to_dict(self, numpy_action, no_batch=False):
-        dict_numpy_action = {
-            "buttons": numpy_action[:-2], "camera": numpy_action[-2:]}
-        if not no_batch:
-            dict_numpy_action = self.action_mapper.to_factored(
-                dict_numpy_action)
-        dict_action = self.action_transformer.numpy_to_dict(dict_numpy_action)
-        dict_action["ESC"] = np.array(0)
-        dict_action["swapHands"] = np.array(0)
-        dict_action["pickItem"] = np.array(0)
-        ordered_keys = self.env.action_space.spaces.keys()
-        dict_action = OrderedDict(
-            {key: dict_action[key] for key in ordered_keys})
-        return dict_action
-    """
-
-    def _convert_to_box_action_space(self, action_space):
-        lower_bounds = []
-        higher_bounds = []
-        box_lower_bounds = []
-        box_higher_bounds = []
-        for name, space in action_space.spaces.items():
-            if name in ["pickItem", "swapHands", "ESC"]:
-                continue
-            if isinstance(space, Discrete):
-                lower_bounds.append(0)
-                higher_bounds.append(space.n - 1)
-            elif isinstance(space, Box):
-                box_lower_bounds.extend(space.low)
-                box_higher_bounds.extend(space.high)
-            else:
-                raise ValueError(f"Space type {type(space)} is not supported.")
-        # dtype=np.int64 because this the output of the CameraQuantizer
-        return Box(low=np.array(lower_bounds + box_lower_bounds), high=np.array(higher_bounds + box_higher_bounds), dtype=np.int64)
+    def to_agent_action(self, array_action):
+        agent_action = {
+            "camera": array_action[..., 0], "buttons": array_action[..., 1]}
+        return agent_action
 
     def step(self, action):
-        # transform vector action to dict action to MineRL action
-        dict_action = {"buttons": action[..., 0], "camera": action[..., 1]}
-        minerl_action = self.minerl_agent._agent_action_to_env(dict_action)
+        # transform array action to agent action to MineRL action
+        agent_action = self.to_agent_action(action)
+        minerl_action = self.minerl_agent._agent_action_to_env(agent_action)
 
-        # TODO implement policy that controls the remaining actions:
+        # TODO implement policy that controls the remaining actions (especially ESC):
         minerl_action["ESC"] = np.array(0)
-        minerl_action["swapHands"] = np.array(0)
-        minerl_action["pickItem"] = np.array(0)
+        minerl_action["swapHands"] = minerl_action["pickItem"] = np.array(0)
 
         obs, reward, terminated, info = self.env.step(minerl_action)
 
@@ -119,14 +91,18 @@ class DictToBoxActionSpace(gym.Wrapper):
 
 
 class HiddenStateObservationSpace(gym.Wrapper):
+    """Augments the observation space by the hidden state of the MineRLAgent"""
+
     def __init__(self, env, minerl_agent):
         super().__init__(env)
+
         self.minerl_agent = minerl_agent
 
         # TODO determine state_in shapes from architecture
         img_shape = self.minerl_agent._env_obs_to_agent(
             self.env.observation_space.sample())["img"].shape
         first_shape = self.minerl_agent._dummy_first.shape
+
         self.observation_space = Dict({
             "img": Box(-10, 10, shape=img_shape),
             "first": Box(-10, 10, shape=first_shape),
