1ef2aca7a089aab47dacdbc97d871ed265c1405c
rk1a <robertklassert@pm.me>
2022-10-11 21:11:18 +0200

    Use CnnRewardNet and PrefCollectGatherer

--- preference_based_RL.py
+++ preference_based_RL.py
@@ -5,7 +5,11 @@ import torch as th
 import gym
 from stable_baselines3 import PPO
 from imitation.algorithms import preference_comparisons
-from imitation.rewards.reward_nets import BasicRewardNet
+from imitation.rewards.reward_nets import (
+    BasicRewardNet,
+    CnnRewardNet,
+    NormalizedRewardNet,
+)
 from imitation.util.networks import RunningNorm
 from imitation.util.util import make_vec_env
 import minerl  # noqa: F401
@@ -50,22 +54,27 @@ def preference_based_RL_train(env_str, in_model, in_weights, out_weights):
 
     # TODO use a suitable reward model architecture,
     # e.g. reuse ImpalaCNN from the VPT models with a regression head
-    reward_net = BasicRewardNet(
-        venv.observation_space, venv.action_space, normalize_input_layer=RunningNorm
+    reward_net = CnnRewardNet(
+        venv.observation_space["img"], venv.action_space, use_action=False
     )
-    preference_model = preference_comparisons.PreferenceModel(reward_net)
+    normalized_reward_net = NormalizedRewardNet(reward_net, RunningNorm)
+    preference_model = preference_comparisons.PreferenceModel(normalized_reward_net)
 
     # TODO design more useful fragmenter for MineRL trajcetories,
     # e.g. only compare last parts of episodes
     fragmenter = preference_comparisons.RandomFragmenter(warning_threshold=0, seed=0)
 
     # TODO design gatherer that obtains preferences from web interface
-    gatherer = preference_comparisons.SyntheticGatherer(seed=0)
+    # gatherer = preference_comparisons.SyntheticGatherer(seed=0)
+    gatherer = preference_comparisons.PrefCollectGatherer(
+        pref_collect_address="http://127.0.0.1:8000",
+        video_output_dir="/home/aicrowd/videofiles/",
+    )
 
     # TODO imitation also provides EnsembleTrainer
     # (which requires a RewardEnsemble), which trainer should we use?
     reward_trainer = preference_comparisons.BasicRewardTrainer(
-        model=reward_net,
+        model=normalized_reward_net,
         loss=preference_comparisons.CrossEntropyRewardLoss(preference_model),
         epochs=3,
     )
@@ -78,14 +87,14 @@ def preference_based_RL_train(env_str, in_model, in_weights, out_weights):
         },
         env=venv,
         seed=0,
-        n_steps=2048 // venv.num_envs,
-        batch_size=64,
+        n_steps=512 // venv.num_envs,
+        batch_size=32,
         ent_coef=0.0,
         learning_rate=0.0003,
         n_epochs=10,
     )
 
-    trajectory_generator = preference_comparisons.AgentTrainer(
+    trajectory_generator = preference_comparisons.MineRLAgentTrainer(
         algorithm=agent,
         reward_fn=reward_net,
         venv=venv,
@@ -110,8 +119,8 @@ def preference_based_RL_train(env_str, in_model, in_weights, out_weights):
 
     # Run training
     pref_comparisons.train(
-        total_timesteps=5_000,  # For good performance this should be 1_000_000
-        total_comparisons=200,  # For good performance this should be 5_000
+        total_timesteps=500,  # For good performance this should be 1_000_000
+        total_comparisons=10,  # For good performance this should be 5_000
     )
 
     venv.close()
