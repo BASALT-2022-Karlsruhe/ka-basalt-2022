7698d13c12fb213f6372fc8b4935d91219a360c3
Walter Laurito <Lauritowal@yahoo.com>
2022-10-12 16:32:27 +0200

    cast to string

--- utils/env_handler.py
+++ utils/env_handler.py
@@ -2,5 +2,5 @@ import os
 
 
 def set_env(eval_episodes=10, eval_max_steps=1000):
-    os.environ["AICROWD_NUM_EVAL_EPISODES"] = eval_episodes
-    os.environ["AICROWD_NUM_EVAL_MAX_STEPS"] = eval_max_steps
+    os.environ["AICROWD_NUM_EVAL_EPISODES"] = str(eval_episodes)
+    os.environ["AICROWD_NUM_EVAL_MAX_STEPS"] = str(eval_max_steps)
