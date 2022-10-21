47db0cc19ca21811d10128b2722621cfaefd70c1
rk1a <robertklassert@pm.me>
2022-10-21 10:58:08 +0200

    Use wandb team for concurrent use

--- train.py
+++ train.py
@@ -10,9 +10,8 @@ from preference_based_RL import preference_based_RL_train
 from utils.logs import Logging
 from utils.create_videos import create_videos
 
-# from utils.visualizer import visualize_loss
 
-FOUNDATION_MODEL = "foundation-model-2x"
+FOUNDATION_MODEL = "foundation-model-1x"
 BC_TRAINING = True
 PREFRL_TRAINING = False
 ENVS = ["FindCave", "MakeWaterfall", "CreateVillageAnimalPen", "BuildVillageHouse"]
@@ -37,13 +36,13 @@ def post_training():
     """
     executed after training  # Add things you want to execute
     """
-    # Logging.info("Saving loss plot...")
-    # visualize_loss(log_file_path=f"/home/aicrowd/train/{LOG_FILE}")
 
     Logging.info("Creating videos...")
     for i, env in enumerate(ENVS):
         model = "BehavioralCloning" if not PREFRL_TRAINING else "PreferenceBasedRL"
-        create_videos(model, env, FOUNDATION_MODEL, NUM_VIDEOS, NUM_MAX_STEPS[i], show=False)
+        create_videos(
+            model, env, FOUNDATION_MODEL, NUM_VIDEOS, NUM_MAX_STEPS[i], show=False
+        )
     Logging.info("End training")
 
 
@@ -52,7 +51,9 @@ def main():
 
     if BC_TRAINING:
         for env in ENVS:
-            run = wandb.init(project=f"BC Training {env}", reinit=True)
+            run = wandb.init(
+                project=f"BC Training {env}", reinit=True, entity="kabasalt_team"
+            )
             Logging.info(f"===BC Training {env} model===")
             behavioural_cloning_train(
                 data_dir=f"data/MineRLBasalt{env}-v0",
@@ -74,6 +75,7 @@ def main():
                 reinit=True,
                 sync_tensorboard=True,
                 monitor_gym=True,
+                entity="kabasalt_team",
             )
             Logging.info(f"===PrefRL Training {env} model===")
             preference_based_RL_train(
