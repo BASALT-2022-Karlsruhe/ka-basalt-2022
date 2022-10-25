a45189d154fd8d0c1c824c3d18d63f3742e3693d
rk1a <robertklassert@pm.me>
2022-10-25 17:39:59 +0200

    Add batch dim if necessary

--- impala_based_models.py
+++ impala_based_models.py
@@ -15,9 +15,10 @@ from behavioural_cloning import load_model_parameters
 class ImpalaLinear(nn.Module):
     """ImpalaCNN followed by a linear layer.
 
-    :param output_size: output size of the linear layer.
     :param cnn_outsize: impala output dimension
-    :param cnn_width: width ImpalaCNN
+    :param output_size: output size of the linear layer.
+    :param dense_init_norm_kwargs: kwargs for linear FanInInitReLULayer
+    :param init_norm_kwargs: kwargs for 2d and 3d conv FanInInitReLULayer
     """
 
     def __init__(
@@ -38,6 +39,7 @@ class ImpalaLinear(nn.Module):
             raise ValueError(f"There is no VPT model with width {model_width}!")
         self.cnn_width = cnn_width
 
+        # TODO use img_preprocess?
         self.img_preprocess = ImgPreprocessing(img_statistics=None, scale_img=True)
         self.cnn = ImpalaCNN(
             inshape=[128, 128, 3],
@@ -57,9 +59,12 @@ class ImpalaLinear(nn.Module):
         )
 
     def forward(self, img):
-        # Need to add fictitious time dimension
-        if len(img.shape[:-3]) < 2:
+        if len(img.shape[:-3]) == 1:
+            # Need to add fictitious time dimension
             img = img.unsqueeze(1)
+        elif len(img.shape[:-3]) == 0:
+            # Need to add batch and time dimension
+            img = img.unsqueeze(0).unsqueeze(0)
         out = self.linear(self.cnn(self.img_preprocess(img)))
         # Remove fictitious time dimension
         out = out.squeeze(1)
@@ -82,19 +87,27 @@ class ImpalaBinaryClassifier(nn.Module):
     def forward(self, obs):
         return self.out_linear(self.impala_linear(obs))
 
+    @th.no_grad()
+    def class_probs(self, obs):
+        return nn.functional.softmax(self.forward(obs), dim=-1)
 
-class ImpalaRegressor(nn.Module):
-    """Regression network based on ImpalaCNN"""
+    @th.no_grad()
+    def predict(self, obs):
+        return self.forward(obs).argmax(dim=-1)
 
-    def __init__(self, cnn_outsize=256, cnn_width=1, model_path="data/VPT-models", hidden_size=256):
+
+class ImpalaRewardModel(nn.Module):
+    """Reward model based on ImpalaCNN"""
+
+    def __init__(self, cnn_outsize=256, cnn_width=1, model_path="data/VPT-models"):
         super().__init__()
-        self.impala_linear = ImpalaLinear(hidden_size, cnn_outsize, cnn_width)
-        self.out_linear = nn.Linear(hidden_size, 1)
+        self.impala_linear = ImpalaLinear(cnn_outsize, 1, cnn_width)
         self.impala_linear.load_cnn_weights(model_path)
-        self.hidden_size = hidden_size
+        # TODO normalizing layer
+        #self.normalized_reward_layer = nn.Linear(hidden_size, 1)
 
     def forward(self, obs):
-        return self.out_linear(self.impala_linear(obs))
+        return self.impala_linear(obs)
 
 
 def save_impala_cnn_weights(model_width=1):
@@ -129,7 +142,7 @@ def load_impala_cnn_weights(
     model_width=1,
     weights_path="data/VPT-models/ImpalaCNN-1x.weights",
 ):
-    """Load previously saved ImpalaCNN weights into a new model object"""
+    """Load previously saved"""
 
     if model_width == 1:
         chans=(64, 128, 128)
