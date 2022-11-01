2664bc1d139aa879779e46bfee2d838653398b05
rk1a <robertklassert@pm.me>
2022-11-01 17:03:52 +0000

    Cleanup unused files

--- train_RL_policy.py
+++
@@ -1,8 +0,0 @@
-from RL_policy_finetuning import PPO_policy_finetuning
-
-if __name__ == "__main__":
-    PPO_policy_finetuning(
-        in_model="data/VPT-models/foundation-model-1x.model",
-        in_weights="data/VPT-models/foundation-model-1x.weights",
-        out_weights="train/MineRLBasaltFindCave.weights"
-    )
